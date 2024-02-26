1ec29c985cdd3aa365004f157da490c3616f20af
zmm-fe <543597191@qq.com>
2024-02-26 17:09:44 +0800

    fix: mobile emvironment use crossOrigin anonymous need set random query

--- src/core/cache-storage.ts
+++ src/core/cache-storage.ts
@@ -96,6 +96,16 @@ export class Cache {
             //ios safari 10.3 taints canvas with data urls unless crossOrigin is set to anonymous
             if (isInlineBase64Image(src) || useCORS) {
                 img.crossOrigin = 'anonymous';
+                if (!isInlineBase64Image(src)) {
+                    try {
+                        const imgUrl = new window.URL(src);
+                        const params = new URLSearchParams(imgUrl.search);
+                        params.set('_t', Date.now() + '');
+                        src = imgUrl.origin + imgUrl.pathname + '?' + params;
+                    } catch (error) {
+                        reject(error);
+                    }
+                }
             }
             img.src = src;
             if (img.complete === true) {
